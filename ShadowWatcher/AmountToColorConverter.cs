67ab10955b4500abd073dfd11cc461fb156e4c48
Gizeta <0w0@gizeta.tk>
2017-01-25 04:38:06 +0900

    Make text colorful

---
+++ ShadowWatcher/AmountToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ShadowWatcher
+{
+    public class AmountToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((int)value)
+            {
+                case 1:
+                    return new SolidColorBrush(Colors.Black);
+                case 2:
+                    return new SolidColorBrush(Colors.Orange);
+                default:
+                    return new SolidColorBrush(Colors.Red);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
